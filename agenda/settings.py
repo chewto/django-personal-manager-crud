0a7408faff668f217b0e5c9fa0658ca9b5849d40
chewto <jesuselozada@gmail.com>
2022-09-17 11:12:04 -0400

    help

--- agenda/settings.py
+++ agenda/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i)!do)ap5d9^exmczh2!#t+)kf-yhjtj*+4_**x6&@hu11(u^1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
